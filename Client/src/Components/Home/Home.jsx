7a6fdcb10f839f65073701dea07a52cddb14b6fa
santiago mojica <samojica08@gmail.com>
2023-11-14 09:15:51 -0500

    Vista del home

--- Client/src/Components/Home/Home.jsx
+++ Client/src/Components/Home/Home.jsx
@@ -1,15 +1,100 @@
 import React from 'react';
 import "./Home.css"
-import Header from '../Header/Header';
-import Footer from './../Footer/Footer';
-
+import EditIcon from '@mui/icons-material/Edit';
+import { useEffect, useState } from 'react';
 const Home = () => {
-    return (
-        <>
-            <Header />
 
-            <Footer />
-        </>
+    const [datos, setDatos] = useState();
+
+    useEffect(() => {
+        const fetchData = async () => {
+            try {
+                const response = await fetch('http://127.0.0.1:8000/animales');
+                const data = await response.json();
+                setDatos(data);
+            } catch (error) {
+                console.error('Error al obtener datos de la API', error);
+            }
+        };
+
+        fetchData();
+    }, []);
+
+    console.log(datos)
+    return (
+        <div className="contenedor">
+
+            <aside>
+                <h1>Fincapp</h1>
+                <ul>
+                    <li>Lechería</li>
+                    <li>Genética</li>
+                    <li>Engorde</li>
+                    <li>Corrales</li>
+                    <li>Personal</li>
+                    <li>Ventas</li>
+                    <li>Rentabilidad</li>
+                </ul>
+            </aside>
+            <main>
+                <div className="contenedor_main">
+                    <h1>Lechería</h1>
+                    <div className='contenedor_tabla'>
+                        <table>
+                            <thead>
+                                <tr>
+                                    <th>Id</th>
+                                    <th>Raza</th>
+                                    <th>Cantidad</th>
+                                    <th>Edad</th>
+                                    <th>Corral</th>
+                                </tr>
+                            </thead>
+                            <tbody>
+                                <tr>
+                                    <td>A1</td>
+                                    <td>Holstein</td>
+                                    <td>25 litros</td>
+                                    <td>35 meses</td>
+                                    <td>C1</td>
+                                    <EditIcon />
+                                </tr>
+                                <tr>
+                                    <td>A1</td>
+                                    <td>Holstein</td>
+                                    <td>25 litros</td>
+                                    <td>35 meses</td>
+                                    <td>C1</td>
+                                    <EditIcon />
+                                </tr>
+                                <tr>
+                                    <td>A1</td>
+                                    <td>Holstein</td>
+                                    <td>25 litros</td>
+                                    <td>35 meses</td>
+                                    <td>C1</td>
+                                    <EditIcon />
+                                </tr>
+                                <tr>
+                                    <td>A1</td>
+                                    <td>Holstein</td>
+                                    <td>25 litros</td>
+                                    <td>35 meses</td>
+                                    <td>C1</td>
+                                    <EditIcon />
+                                </tr>
+
+
+
+                            </tbody>
+                        </table>
+                    </div>
+                    <button>Agregar</button>
+
+                </div>
+
+            </main>
+        </div>
     );
 }
 
