a0b4eabb8f610064aad2eacfb98156336e63c1c8
santiago mojica <samojica08@gmail.com>
2024-08-22 18:12:56 -0500

    Menu hamburguesa del diseño movil

---
+++ Frontend/src/components/Hamburguesa.jsx
@@ -0,0 +1,31 @@
+import { useState } from 'react';
+import Menu from './Menu.jsx';
+
+
+function Hamburguesa() {
+    const [menu, setMenu] = useState(false)
+    const [icono, setIcono] = useState("./menu.svg");
+
+    function handleClick() {
+        if (icono == "./menu.svg") {
+            setIcono("./cerrar.svg")
+            setMenu(true)
+        } else {
+            setIcono("./menu.svg")
+            setMenu(false)
+        }
+
+    }
+
+    return (
+        <>
+            <div className="flex sm:hidden justify-center" onClick={handleClick}>
+                <img className="w-6 h-6" src={icono} alt="Menu hamburguesa" />
+            </div>
+            {menu && <Menu />}
+        </>
+    );
+
+}
+
+export default Hamburguesa;
