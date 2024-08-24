5cf0bb3ab6d4bfc932a816f10760fbd91b31e68a
santiago mojica <samojica08@gmail.com>
2024-08-24 13:27:22 -0500

    Dark mode landing

--- Frontend/src/components/Header/Menu.jsx
+++ Frontend/src/components/Header/Menu.jsx
@@ -1,7 +1,7 @@
 function Menu() {
     return (
-        <div class="absolute top-20 right-0 pt-6 flex flex-col w-1/2 h-screen rounded-lg bg-[var(--secundario)] justify-start gap-3 sm:hidden">
-            <div className="flex justify-center rounded-lg h-10 mx-5 bg-[var(--primario)] items-center"><a className="flex justify-center w-full text-center" href="#inicio">Inicio</a></div>
+        <div class="absolute top-20 right-4 pt-6 flex flex-col w-1/2 h-screen rounded-lg bg-[var(--secundario)] justify-start gap-3 sm:hidden">
+            <div className="flex justify-center rounded-lg h-10 mx-5 bg-[var(--primario)] items-center "><a className="flex justify-center w-full text-center " href="#inicio">Inicio</a></div>
             <div className="flex justify-center rounded-lg h-10 mx-5 bg-[var(--primario)] items-center"><a className="flex justify-center w-full text-center" href="#acerca">Acerca</a></div>
             <div className="flex justify-center rounded-lg h-10 mx-5 bg-[var(--primario)] items-center"><a className="flex justify-center w-full text-center" href="#quien-soy">Quien soy?</a></div>
             <div className="flex justify-center rounded-lg h-10 mx-5 bg-[var(--primario)] items-center"><a className="flex justify-center w-full text-center" href="#contacto">Contacto</a></div>
--- Frontend/tailwind.config.mjs
+++ Frontend/tailwind.config.mjs
@@ -3,11 +3,10 @@ export default {
     content: ["./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}"],
     darkMode: "class",
     theme: {
-        extend: {
-            container: {
-                center: true,
-            },
-        },
+        extend: {},
+    },
+    variants: {
+        extend: {},
     },
     plugins: [],
 };
